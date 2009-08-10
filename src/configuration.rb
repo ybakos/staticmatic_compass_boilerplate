a9e709f8d0c622d21fc00e8e097699fddb007673
Yong Bakos <projects@humanoriented.com>
2009-08-10 13:05:27 -0600

    Initial commit of staticmatic and compass configuration.

---
+++ src/configuration.rb
@@ -0,0 +1,10 @@
+require 'compass'
+
+configuration.haml_options = { :format => :html4 }
+
+Compass.configuration do |config|
+  config.project_path = File.dirname(__FILE__)
+  config.sass_dir = File.join('stylesheets')
+end
+
+configuration.sass_options = Compass.sass_engine_options
